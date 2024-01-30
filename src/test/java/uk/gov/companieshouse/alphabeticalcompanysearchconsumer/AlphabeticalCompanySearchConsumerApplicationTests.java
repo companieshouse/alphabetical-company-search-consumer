8c1d64fafef032c8e4a0528de698775b8856a665
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2024-01-30 09:50:47 +0000

    BI-13537: Replace tabs with 4 spaces.

--- src/test/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/AlphabeticalCompanySearchConsumerApplicationTests.java
+++ src/test/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/AlphabeticalCompanySearchConsumerApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AlphabeticalCompanySearchConsumerApplicationTests {
 
-	@SuppressWarnings("squid:S2699") // at least one assertion
-	@DisplayName("Context loads")
-	@Test
-	void contextLoads() {
-	}
+    @SuppressWarnings("squid:S2699") // at least one assertion
+    @DisplayName("Context loads")
+    @Test
+    void contextLoads() {
+    }
 
 }
