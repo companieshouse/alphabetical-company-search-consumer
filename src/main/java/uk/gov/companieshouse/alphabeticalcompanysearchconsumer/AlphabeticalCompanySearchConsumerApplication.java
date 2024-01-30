4289187f001497f2c91818ff36bf6220cee18a3d
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2024-01-30 08:41:11 +0000

    BI-13537: Add basic skeleton app.

---
+++ src/main/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/AlphabeticalCompanySearchConsumerApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.companieshouse.alphabeticalcompanysearchconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphabeticalCompanySearchConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphabeticalCompanySearchConsumerApplication.class, args);
+	}
+
+}
