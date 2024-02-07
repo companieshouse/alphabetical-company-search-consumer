35ce0142143ef5105596646098f2e1a144ed87d2
rmcmurray-ch <rmcmurray@companieshouse.gov.uk>
2024-02-07 11:32:06 +0000

    attempt symbolic link

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -19,7 +19,7 @@ terraform {
 }
 
 module "secrets" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.216"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/secrets?ref=1.0.245"
 
   name_prefix = "${local.service_name}-${var.environment}"
   environment = var.environment
