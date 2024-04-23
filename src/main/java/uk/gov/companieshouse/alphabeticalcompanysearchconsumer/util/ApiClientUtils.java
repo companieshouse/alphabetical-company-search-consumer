395cfea7ce21c6f4488455cabb7f43b93234860c
aokunade <aokunade@companieshouse.gov.uk>
2024-04-23 12:37:22 +0100

    more uniform logging in place of a stack trace

--- src/main/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/util/ApiClientUtils.java
+++ src/main/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/util/ApiClientUtils.java
@@ -1,26 +1,29 @@
 package uk.gov.companieshouse.alphabeticalcompanysearchconsumer.util;
 
+import uk.gov.companieshouse.alphabeticalcompanysearchconsumer.exception.NonRetryableException;
 import uk.gov.companieshouse.api.model.company.CompanyProfileApi;
 import uk.gov.companieshouse.stream.ResourceChangedData;
 import java.io.IOException;
-
+import static uk.gov.companieshouse.alphabeticalcompanysearchconsumer.logging.LoggingUtils.getRootCause;
+import uk.gov.companieshouse.logging.Logger;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class ApiClientUtils {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static Logger logger = null;
 
     public static CompanyProfileApi mapMessageToRequest(ServiceParameters parameters) {
         ResourceChangedData data = parameters.getData();
-        String jsonData = data.getData();
-        
+        String jsonData = data.getData();        
         try {
             return objectMapper.readValue(jsonData, CompanyProfileApi.class);
         } catch (IOException e) {
             // Handle the exception appropriately
-            e.printStackTrace();
-            return null; // or throw a custom exception
+            final var rootCause = getRootCause(e);
+            logger.error(String.format("NonRetryable Error: %s", rootCause));
+            throw new NonRetryableException("AlphabeticalIndexUpdaterService.processMessage: ", rootCause);
         }
     }
 }
