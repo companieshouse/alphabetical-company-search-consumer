f90132f264e3d6a00bd2abf08a8976f12732d50d
artemisgardnerch <agardner@companieshouse.gov,uk>
2026-01-13 11:30:25 +0000

    update dependencies for the final time, parking task

--- src/main/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/config/Config.java
+++ src/main/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/config/Config.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+import org.springframework.kafka.annotation.BackOff;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
