0deec6090dea07430a62f47e0dd14d76c11abc74
aokunade <aokunade@companieshouse.gov.uk>
2026-05-12 10:51:53 +0100

    Fix failing pipeline test

--- src/test/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/config/ProducerSerializationExceptionTest.java
+++ src/test/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/config/ProducerSerializationExceptionTest.java
@@ -49,7 +49,6 @@ class ProducerSerializationExceptionTest extends AbstractKafkaIntegrationTest {
     @BeforeEach
     public void drainKafkaTopics() {
         testConsumer.poll(Duration.ofSeconds(1));
-        testConsumer.poll(Duration.ofMillis(500));
     }
 
     @Test
