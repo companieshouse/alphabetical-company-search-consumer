d750477f5ae4c2a8b93a7d2530b1c8410b000acb
jbishop2-ch <jbishop2@companieshouse.gov.uk>
2026-09-11 12:23:14 +0100

    Increased test-coverage to 99% on code refactoring

--- src/test/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/service/ServiceParametersTest.java
+++ src/test/java/uk/gov/companieshouse/alphabeticalcompanysearchconsumer/service/ServiceParametersTest.java
@@ -2,7 +2,6 @@ package uk.gov.companieshouse.alphabeticalcompanysearchconsumer.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
